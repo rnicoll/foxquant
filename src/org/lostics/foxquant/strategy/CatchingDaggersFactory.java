6beea415bf08bbcbae98a87d33d899d4584603f6
Ross Nicoll <jrn.me.uk>
2009-11-17 00:28:03 +0000

    Completed methods in CatchingDaggersFactory for requesting and receiving permission to go long/short on currencies.

--- src/org/lostics/foxquant/strategy/CatchingDaggersFactory.java
+++ src/org/lostics/foxquant/strategy/CatchingDaggersFactory.java
@@ -3,11 +3,14 @@ package org.lostics.foxquant.strategy;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.lostics.foxquant.model.ContractManager;
 import org.lostics.foxquant.model.StrategyAlreadyExistsException;
+import org.lostics.foxquant.model.StrategyException;
 import org.lostics.foxquant.model.StrategyFactory;
 import org.lostics.foxquant.model.Strategy;
 import org.lostics.foxquant.Configuration;
@@ -25,8 +28,8 @@ public class CatchingDaggersFactory implements StrategyFactory {
     public static final double DEFAULT_SPREAD = 2;
     
     // Synchronize access to these maps, on themselves.
-    private final Map<String, CatchingDaggers> longStrategies = new HashMap<String, CatchingDaggers>();
-    private final Map<String, CatchingDaggers> shortStrategies = new HashMap<String, CatchingDaggers>();
+    private final Map<String, LinkedHashSet<CatchingDaggers>> longStrategies = new HashMap<String, LinkedHashSet<CatchingDaggers>>();
+    private final Map<String, LinkedHashSet<CatchingDaggers>> shortStrategies = new HashMap<String, LinkedHashSet<CatchingDaggers>>();
     
     private int historicalBars = DEFAULT_HISTORICAL_BARS;
     private double spread = DEFAULT_SPREAD;
@@ -67,27 +70,131 @@ public class CatchingDaggersFactory implements StrategyFactory {
         return strategy;
     }
     
-    protected boolean requestLong(final String symbol, final CatchingDaggers requestor) {
+    /**
+     * Class private method for removing a strategy from a request queue (list),
+     * and retrieving the strategy at the front of the queue ONLY if it changes.
+     * MUST be called fron within a synchronized block on the map being
+     * affected.
+     *
+     * @return a strategy to notify it has perimission to go long/short. Will be
+     * null if EITHER no strategies are waiting for permission OR if the strategy
+     * at the front of the queue has not changed.
+     */
+    private CatchingDaggers cancelRequestAndGetFirst(final LinkedHashSet<CatchingDaggers> strategies, final CatchingDaggers requestor)
+        throws StrategyException {
+        final Iterator<CatchingDaggers> iterator;
+        final Strategy originalNextStrategy;
+        final Strategy originalTopStrategy;
+        
+        if (null == strategies) {
+            throw new StrategyException("Cancellation of request to go long, while no strategies have outstanding requests.");
+        }
+        
+        if (strategies.size() == 0) {
+            // Assume a concurrency issue, and ignore.
+            return null;
+        }
+        
+        iterator = strategies.iterator();
+        originalTopStrategy = iterator.next();
+        originalNextStrategy = iterator.next();
+        
+        if (!strategies.remove(requestor)) {
+            // Assume a concurrency issue, and ignore.
+            return null;
+        }
+        if (originalTopStrategy.equals(requestor) &&
+            strategies.size() > 0) {
+            // We've just remove the strategy with the highest priority,
+            // check for another to notify of this change.
+            return (CatchingDaggers)originalNextStrategy;
+        }
+        
+        return null;
+    }
+    
+    protected void cancelRequestLong(final String symbol, final CatchingDaggers requestor)
+        throws StrategyException {
+        CatchingDaggers topStrategy = null;
+        
         synchronized (this.longStrategies) {
-            final CatchingDaggers strategy = this.longStrategies.get(symbol);
-            if (null == strategy) {
-                this.longStrategies.put(symbol, requestor);
-                return true;
-            } else {
-                return false;
-            }
+            final LinkedHashSet<CatchingDaggers> strategies = this.longStrategies.get(symbol);
+            
+            topStrategy = cancelRequestAndGetFirst(strategies, requestor);
+        }
+        
+        if (null != topStrategy) {
+            // XXX: topStrategy.notifyLongRequestApproved();
         }
     }
     
-    protected boolean requestShort(final String symbol, final CatchingDaggers requestor) {
+    protected void cancelRequestShort(final String symbol, final CatchingDaggers requestor)
+        throws StrategyException {
+        CatchingDaggers topStrategy = null;
+        
         synchronized (this.shortStrategies) {
-            final CatchingDaggers strategy = this.shortStrategies.get(symbol);
-            if (null == strategy) {
-                this.shortStrategies.put(symbol, requestor);
+            final LinkedHashSet<CatchingDaggers> strategies = this.shortStrategies.get(symbol);
+            
+            topStrategy = cancelRequestAndGetFirst(strategies, requestor);
+        }
+        
+        if (null != topStrategy) {
+            // XXX: topStrategy.notifyShortRequestApproved();
+        }
+    }
+    
+    
+    /**
+     * Used by CatchingDaggers to request permission to go long on a currency.
+     * If no other strategies are looking to go long on the given currency,
+     * returns true, otherwise returns false. Either way, it adds the strategy
+     * to the queue for that currency's long slot. If returns false, will
+     * call notifyLongRequestApproved() on the strategy once a slot is available,
+     * unless cancelRequestLong() is called on the factory first.
+     */
+    protected boolean requestLong(final String symbol, final CatchingDaggers requestor)
+        throws StrategyException {
+        synchronized (this.longStrategies) {
+            LinkedHashSet<CatchingDaggers> strategies = this.longStrategies.get(symbol);
+            
+            if (null == strategies) {
+                strategies = new LinkedHashSet<CatchingDaggers>();
+                this.longStrategies.put(symbol, strategies);
+            }
+            
+            strategies.add(requestor);
+            if (strategies.size() == 1) {
                 return true;
-            } else {
-                return false;
             }
         }
+        
+        return false;
+    }
+    
+    /**
+     * Used by CatchingDaggers to request permission to go short on a currency.
+     * If no other strategies are looking to go short on the given currency,
+     * returns true, otherwise returns false. Either way, it adds the strategy
+     * to the queue for that currency's short slot. If returns false, will
+     * call notifyShortRequestApproved() on the strategy once a slot is available,
+     * unless cancelRequestShort() is called on the factory first.
+     */
+    protected boolean requestShort(final String symbol, final CatchingDaggers requestor)
+        throws StrategyException {
+        synchronized (this.shortStrategies) {
+            LinkedHashSet<CatchingDaggers> strategies = this.shortStrategies.get(symbol);
+            
+            if (null == strategies) {
+                strategies = new LinkedHashSet<CatchingDaggers>();
+                this.shortStrategies.put(symbol, strategies);
+            }
+            
+            strategies.add(requestor);
+            if (strategies.size() == 1) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
